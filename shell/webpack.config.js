0e8728e27ceb3711859d8b23c8d9d36ee8b53d18
kauaneleeal <kgoncalvesleal@outlook.com>
2025-07-31 23:31:23 -0300

    feat: config root-config

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -9,11 +9,9 @@ module.exports = (webpackConfigEnv, argv) => {
     projectName: "root-config",
     webpackConfigEnv,
     argv,
-    disableHtmlGeneration: true,
   });
 
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
