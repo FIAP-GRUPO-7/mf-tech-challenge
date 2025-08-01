d922b0ea22ca8d42a9e034fe3722eacd2366c15c
kauaneleeal <kgoncalvesleal@outlook.com>
2025-08-01 11:41:54 -0300

    feat: altera configs

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "grupo7";
